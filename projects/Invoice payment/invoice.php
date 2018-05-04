b3759fb596fcf85b2a2cfe8da57a7216e267c159
PragatiMeshram <38005397+PragatiMeshram@users.noreply.github.com>
2018-05-05 00:01:55 +0530

    Update invoice.php

--- projects/Invoice payment/invoice.php
+++ projects/Invoice payment/invoice.php
@@ -1,5 +1,4 @@
 <form method="post" action="payment.php" target="_new">
-
 Total Amount Paid :<input type="text" name="paid"><br/><br/>
 Your Bills : <input type="text" name="bill">
 <input type="submit" name="ok" value="Send">
